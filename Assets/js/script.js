1458535f8053e762db4b71e581f2e0de8852823b
JacobYaws <Jacob.Yaws@du.edu>
2022-11-14 23:42:08 -0700

    Added in comments to the JS file and updated the README

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,34 +1,26 @@
+// These are the global variables that will be called for the functions below. The hour variable is set so that any div that has an id attribute of 'hour' will be affected.
 var headerEl = $('header');
 var saveButton = $('.saveBtn'); 
 var hour = $('div [id^="hour"]');
 
-
-
-
+// Modified the css elements to get the header in the center of the page and give it a tan background.
 headerEl.css('text-align', 'center');
 headerEl.css('background-color', 'tan');
 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {
- 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   
+  // This function will save whatever text content is input to the text area. With the 'this' keyword, the text that is contained within the textarea for each individual button (i.e. the button for 10am will save text for the 10am block, and no others) is saved to
+  // local storage, as well as the id for the div the text was contained in. As a result, whenever the page is refreshed, the text will be rendered onto the page in the text area that it was originally placed in. 
   saveButton.on('click', function() {
     var TextArea = $(this).parent().children(1).eq(1).val();
     localStorage.setItem($(this).parent().attr('id'), (TextArea));
     //console.log($(this).parent().children(1).eq(1));
 });
 
+  // This function tells the code to execute everything within it after the DOM is ready.
   $(document).ready(function() {
+   
+    // This function is used to pull any saved inputs from local storage. Using the 'this' keyword will apply the text to the section on the page it accompanies, which is determined by the key value from local storage. The key values are the 'id' attribute for the divs.
     $("div").each(function(index) {
       if (index != 0) {
         //console.log($(this));
@@ -38,94 +30,44 @@ $(function () {
         var text = localStorage.getItem($(this).attr('id'));
         $(this).children().eq(1).val(text);
         
-        
-
+        // Links the variable to the 'id' elements. 
         let id = $(this).attr('id');
-        if (typeof(id) != 'undefined') {
 
+        // This if statement is meant for 'id' elements that have a value of 'undefined.'
+        if (typeof(id) != 'undefined') {
+        
+        // This variable is set to take the character in postion 5 of the 'id' element and convert that character into a number.
         let hourNumber = parseInt(id.substring(5));
         //console.log(hourNumber);
 
+        // Sets the variable to dayjs, which validates the date and time.
         var today = dayjs();
+        
+        // Sets the part of the page that has an id of 'currentDay' (which is in the header) to display the day, month, and the day number.
         $('#currentDay').text(today.format('dddd, MMMM D'));
 
+        // Sets the variable to the hour of the day (24 hour clock) and converts it to a number.
         var hour = parseInt(today.format('H'));
-        console.log(today.format('H'));
-        console.log(hour);
-        console.log(hourNumber);
-        console.log(hourNumber > hour)
+        //console.log(today.format('H'));
+        //console.log(hour);
+        //console.log(hourNumber);
+        //console.log(hourNumber > hour)
 
-
+        // This if statement takes the hour number of the divs and compares it to the actual hour of the day (this is the reason for converting them to numbers). If the hour number of the div is greater than the actual hour, the section will be given a green background (from the 'future' css selector),
+        // a gray background if the hour number is less than the actual hour ('past' css selector), or a red background if the two variables are equal ('present' css selector).
         if (hourNumber > hour) {
           ($(this).children().eq(1)).addClass('future')
-          console.log(hourNumber);
-          console.log(hour);
-          console.log($(this))
+          //console.log(hourNumber);
+          //console.log(hour);
+          //console.log($(this))
         } else if (hourNumber < hour) {
           ($(this).children().eq(1)).addClass('past')
         } else if (hourNumber == hour) {
           ($(this).children().eq(1)).addClass('present')
         }
-
-
-
         };
-
-      
-
-     
-
-
-      }
-
-    
-
-
-
-      
-
-      //console.log(hourNumber);
-      
-
-        
-       
-      
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //document.getElementsByClassName('.description').innerText = localStorage.getItem($(this).attr('id'));
-    
-        //document.get(attr('id'));
-       /*
-        console.log(" This " + $(this));
-        console.log($(this).children())
-        console.log(" doc " + document.getElementById('hour-9'));
-*/
-
-      
-    });
-    
-  
-  
-  
+      }    
+    }); 
   });
 
 
@@ -161,39 +103,11 @@ $(function () {
 
 
 
-    /*
-    console.log(spTextArea);
-    console.log($(this));
-    console.log($(this).val());
-    console.log(hour);
-    console.log($(this).parent().children(1).eq(1));
-    */
    
    
    
    
-   
-    //$((this)('.description'));
-   
- 
-   
-  
-  
-
-
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+    
 
 
 
